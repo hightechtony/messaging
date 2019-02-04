3ae793a602b1a9debeefb01360c3f29aa238fa99
SalMireles <sal.chemicale@gmail.com>
2019-02-04 11:55:12 -0800

    Initial Commit

---
+++ messaging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messaging
+//
+//  Created by Salvador Mireles on 2/4/19.
+//  Copyright © 2019 Salvador Mireles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
